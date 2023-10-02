70c636eddb524c8ca9603c071c13099544e1ad62
Laura Mañogil <laura.manogil@sembo.se>
2023-10-02 14:08:31 +0200

    vue dragscroll

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './styles/main.scss'
 import App from './App.vue'
+import VueDragscroll from "vue-dragscroll";
 
-createApp(App).mount('#app')
+createApp(App).use(VueDragscroll).mount('#app')
